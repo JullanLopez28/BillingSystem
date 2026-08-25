926558c97f52c8b5edcdbe4d4b2c5633a6a20d36
Jullan Christoper F. Lopez <jullanchristoperfelicidario.lopez25@bicol-u.edu.ph>
2026-08-25 14:20:46 +0800

    Add project files.

---
+++ BillingSystem/CustomerListForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BillingSystem
+{
+    public partial class CustomerListForm : Form
+    {
+        public CustomerListForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // TODO: Add logic to delete the selected customer from dgvCustomers
+            // For now, just show a message box as a placeholder
+            MessageBox.Show("Delete button clicked.");
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            // Add your logout logic here, for example:
+            this.Close();
+        }
+    }
+}
---
+++ BillingSystem/LoginForm.cs
@@ -0,0 +1,15 @@
+namespace BillingSystem
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            // You can add logic here if needed, or leave it empty if not used.
+        }
+    }
+}
