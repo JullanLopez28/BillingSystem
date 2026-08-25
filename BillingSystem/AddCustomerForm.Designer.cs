00bf5da7ecd97fc11eb47428749a112f5e6fc1c0
Jullan Christoper F. Lopez <jullanchristoperfelicidario.lopez25@bicol-u.edu.ph>
2026-08-25 16:15:40 +0800

    Update LoginForm title with version label and my initials

--- BillingSystem/AddCustomerForm.Designer.cs
+++ BillingSystem/AddCustomerForm.Designer.cs
@@ -185,7 +185,7 @@
             MaximizeBox = false;
             Name = "AddCustomerForm";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Billing System - Add Customer";
+            Text = "Billing System v1.0 – Login (J.C.L)";
             ResumeLayout(false);
             PerformLayout();
         }
